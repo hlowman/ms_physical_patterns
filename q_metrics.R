56aadceaccd1fd22e591e6b1b2afc6e1a4f00203
Heili Lowman <heili.lowman@gmail.com>
2024-08-26 13:13:32 -0400

    Fixed AR(1) code and added RBI calculation to close #15.

--- q_metrics.R
+++ q_metrics.R
@@ -83,7 +83,7 @@ q_data_nodup <- q_data_nodup %>%
   mutate(water_year = case_when(month %in% c(10, 11, 12) ~ year+1,
                                 TRUE ~ year))
 
-#### 7 Metrics ####
+#### Q Metrics ####
 
 ##### AR(1) #####
 
@@ -102,13 +102,14 @@ q_data_nodup <- q_data_nodup %>%
   mutate(deseasonQ = val_mmd - lt_meanQ)
 
 # And scale those values to use in the AR(1) calculations.
-q_data_nodup$scaleQds = (q_data_nodup$deseasonQ - mean(q_data_nodup$deseasonQ, na.rm = TRUE))/
+q_data_nodup$scaleQds = (q_data_nodup$deseasonQ - mean(q_data_nodup$deseasonQ,
+                                                       na.rm = TRUE))/
   sd(q_data_nodup$deseasonQ, na.rm = TRUE)
 
 # Function to pull out AR(1) correlation coefficient
-acf_print <- function(x) {
-  a1 <- acf(x, lag.max = 1, plot = FALSE, na.action = na.pass)
-  a1$acf[2]
+ar1_print <- function(x) {
+  a1 <- ar(x, aic = FALSE, order.max = 1, na.action = na.pass)
+  a1$ar
 }
 
 ##### Amplitude/Phase #####
@@ -127,14 +128,58 @@ q_data_nodup <- q_data_nodup %>%
   mutate(sin_2pi_year = sin(2*pi*decimalY),
          cos_2pi_year = cos(2*pi*decimalY))
 
-##### Summary #####
+##### RBI #####
 
-# Create summarized dataset with all 7 metrics.
+# Function to calculate the Richard-Baker flashiness index.
+rbi_print <- function(x) {
+    # Checked to be sure that when grouped, the "diff" function
+    # omits the first record for each site-water year.
+    d <- diff(x)
+    RBI <- sum(abs(d))/sum(x[2:length(x)])
+    return(RBI)
+}
+
+#### Summary ####
+
+# Calculate number of records for each site-water year,
+# since those with too few records will break the
+# regressions below.
+q_wy_counts <- q_data_nodup %>%
+    drop_na(val_mmd) %>%
+    mutate(site_wy = paste(site_code,water_year, sep = "_")) %>%
+    count(site_wy) %>%
+    ungroup() %>%
+    mutate(use = case_when(n > 3 ~ 1,
+                           n <= 3 ~ 0,
+                           TRUE ~ NA))
+# ~75% of site-water years (n = 3,428/4,591) have 360+ days of discharge data
+
+# Also notate sites for which the site-water year mean
+# discharge is zero, which will also not work with the
+# summary calculations below.
+q_wy_mean <- q_data_nodup %>%
+    drop_na(val_mmd) %>%
+    mutate(site_wy = paste(site_code,water_year, sep = "_")) %>%
+    group_by(site_wy) %>%
+    summarize(mean = mean(val_mmd, na.rm = TRUE)) %>%
+    ungroup() %>%
+    mutate(use2 = case_when(mean > 0 ~ 1,
+                           mean <= 0 ~ 0,
+                           TRUE ~ NA))
+
+# Create summarized dataset with all 8 metrics by site-water year.
 q_metrics_siteyear <- q_data_nodup %>%
   # drop all NA discharge values
   # otherwise the lm() with full missing WYs below will throw
   # an error message and not output the summarized data
   drop_na(val_mmd) %>%
+  # also dropping site-water years that broke the regressions' code
+  mutate(site_wy = paste(site_code,water_year, sep = "_")) %>%
+  full_join(q_wy_counts) %>%
+  full_join(q_wy_mean) %>%
+  filter(use == 1) %>%
+  filter(use2 == 1) %>%
+  # finally, calculate the discharge metrics
   group_by(site_code, water_year) %>%
   summarize(m1_meanq = mean(val_mmd, na.rm = TRUE), # mean
             q1 = quantile(val_mmd, probs = 0.01, na.rm = TRUE), # 1st percentile Q
@@ -148,7 +193,8 @@ q_metrics_siteyear <- q_data_nodup %>%
                         mean(val_mmd, na.rm = TRUE)), # coefficient of variation
             m3_skewq = skewness(val_mmd, na.rm = TRUE), # skewness
             m4_kurtq = kurtosis(val_mmd, na.rm = TRUE), # kurtosis
-            m5_ar1q = acf_print(scaleQds), # AR(1) coefficient
+            m5_ar1q = ar1_print(scaleQds), # AR(1) coefficient
+            rbiq = rbi_print(val_mmd), # Richards-Baker flashiness index
             # seasonal flow signal metrics a + b
             # per p. 1169 in Archfield et al., 2014
             a_flow_sig = lm(scaleQ ~ sin_2pi_year + cos_2pi_year,
@@ -162,21 +208,32 @@ q_metrics_siteyear <- q_data_nodup %>%
 # Export data.
 saveRDS(q_metrics_siteyear, "data_working/discharge_7metrics_siteyear.rds")
 
-# Then, summarize discharge metrics by site
-# for easier plotting/visualization.
-
-# Create summarized dataset with all 7 metrics for full time series.
+# Create summarized dataset with all 8 metrics for full time series at each site.
 q_metrics_site <- q_data_nodup %>%
   group_by(site_code) %>%
   # drop all NA discharge values
   # otherwise the lm() will throw an error message
   drop_na(val_mmd) %>%
+  # also dropping site-water years that broke the regressions' code previously
+  mutate(site_wy = paste(site_code,water_year, sep = "_")) %>%
+  full_join(q_wy_counts) %>%
+  full_join(q_wy_mean) %>%
+  filter(use == 1) %>%
+  filter(use2 == 1) %>%
   summarize(m1_meanq = mean(val_mmd, na.rm = TRUE), # mean
+            q1 = quantile(val_mmd, probs = 0.01, na.rm = TRUE), # 1st percentile Q
+            q5 = quantile(val_mmd, probs = 0.05, na.rm = TRUE), # 5th percentile Q
+            q25 = quantile(val_mmd, probs = 0.25, na.rm = TRUE), # 25th percentile Q
+            q50 = quantile(val_mmd, probs = 0.50, na.rm = TRUE), # median Q
+            q75 = quantile(val_mmd, probs = 0.75, na.rm = TRUE), # 75th percentile Q
+            q95 = quantile(val_mmd, probs = 0.95, na.rm = TRUE), # 95th percentile Q
+            q99 = quantile(val_mmd, probs = 0.99, na.rm = TRUE), # 99th percentile Q
             m2_cvq = (sd(val_mmd, na.rm = TRUE)/
                         mean(val_mmd, na.rm = TRUE)), # coefficient of variation
             m3_skewq = skewness(val_mmd, na.rm = TRUE), # skewness
             m4_kurtq = kurtosis(val_mmd, na.rm = TRUE), # kurtosis
             m5_ar1q = acf_print(scaleQds), # AR(1) coefficient
+            rbiq = rbi_print(val_mmd), # Richards-Baker flashiness index
             a_flow_sig = lm(scaleQ ~ sin_2pi_year + cos_2pi_year,
                             na.action = na.omit)$coefficients["sin_2pi_year"],
             b_flow_sig = lm(scaleQ ~ sin_2pi_year + cos_2pi_year,
@@ -186,6 +243,6 @@ q_metrics_site <- q_data_nodup %>%
          m7_phiq = atan(-a_flow_sig/b_flow_sig)) # phase shift
 
 # Export data.
-#saveRDS(q_metrics_site, "data_working/discharge_7metrics.rds")
+saveRDS(q_metrics_site, "data_working/discharge_7metrics.rds")
 
 # End of script.
