b11d8ae6ee02f6ce83998e9dc1c8f4c2c680e2b2
ecogub <69318415+ecogub@users.noreply.github.com>
2024-08-30 10:12:57 -0600

    made setup script and moved contents out of others

--- q_metrics.R
+++ q_metrics.R
@@ -8,40 +8,8 @@
 # discharge metrics (Archfield et al., 2014) as well as
 # the Richards-Baker Flashiness Index (Baker et al., 2004)
 # for all MacroSheds sites at which data is currently available.
-
-# Note, this script uses a newer version of the data (v2) sent by
-# Mike on 8/19/24, not the current package version of the dataset.
-
-#### Setup ####
-
-# Load packages.
 library(here)
-library(tidyverse)
-library(ggplot2)
-library(naniar)
-library(lubridate)
-library(feather)
-library(macrosheds)
-library(moments)
-
-# Using revised code from Mike to point to new data.
-rdata_path <- "data_raw" # updated path
-
-load(file.path(rdata_path, 'ms_site_data.RData'))
-load(file.path(rdata_path, 'ms_vars_ws_attr.RData'))
-load(file.path(rdata_path, 'ms_vars_ts.RData'))
-load(file.path(rdata_path, 'ms_var_catalog.RData'))
-
-nv <- as.environment('package:macrosheds')
-
-for(ms_data in c('ms_vars_ts', 'ms_vars_ws', 'ms_site_data', 'ms_var_catalog')){
-  unlockBinding(ms_data, nv)
-  assign(ms_data, get(ms_data), envir = nv)
-  lockBinding(ms_data, nv)
-}
-
-# Set directory to folder containing new data.
-my_ms_dir <- "data_raw"
+source(here('src', 'setup.R'))
 
 # Load dataset - be patient, takes just a moment.
 q_data <- ms_load_product(
---
+++ src/setup.R
@@ -0,0 +1,41 @@
+# setup script to save effort when using v2 without package
+
+# Note, this script uses a newer version of the data (v2) sent by
+# Mike on 8/19/24, not the current package version of the dataset.
+
+# Load common packages.
+library(here)
+library(tidyverse)
+library(ggplot2)
+library(naniar)
+library(lubridate)
+library(feather)
+library(macrosheds)
+library(feather)
+library(lfstat)
+library(trend)
+library(ggrepel)
+library(ggthemes)
+library(viridis)
+library(patchwork)
+library(ggforce)
+
+
+# Using revised code from Mike to point to new data.
+rdata_path <- "data_raw" # updated path
+
+load(file.path(rdata_path, 'ms_site_data.RData'))
+load(file.path(rdata_path, 'ms_vars_ws_attr.RData'))
+load(file.path(rdata_path, 'ms_vars_ts.RData'))
+load(file.path(rdata_path, 'ms_var_catalog.RData'))
+
+nv <- as.environment('package:macrosheds')
+
+for(ms_data in c('ms_vars_ts', 'ms_vars_ws', 'ms_site_data', 'ms_var_catalog')){
+    unlockBinding(ms_data, nv)
+    assign(ms_data, get(ms_data), envir = nv)
+    lockBinding(ms_data, nv)
+}
+
+# Set directory to folder containing new data.
+my_ms_dir <- "data_raw"
