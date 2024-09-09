026f9b0c83d5cc4d376c208daae18310cb2575c2
ecogub <69318415+ecogub@users.noreply.github.com>
2024-09-09 10:28:44 -0600

    moved master coverage var definition to own script

---
+++ src/set_master_coverage_vars.R
@@ -0,0 +1,6 @@
+# set master vars for data coverage to define 'good years'
+# shared across all data coverage scripts
+start_year_master <- 2001
+minimum_per_week_sampling_frequency_master <- 4
+good_weeks_to_year_master <- 51
+minimum_continuous_record_length_master <- 10
